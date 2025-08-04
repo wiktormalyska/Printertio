0a8ff7b2115a40b732147b4660570014e89e4a82
wiktormalyska <wiktormalyska03@gmail.com>
2025-08-04 23:18:06 +0200

    Add Create Discount Form component with enhanced attributes and logic for discount management

--- force-app/main/default/aura/createDiscountForm/createDiscountFormController.js
+++ force-app/main/default/aura/createDiscountForm/createDiscountFormController.js
@@ -1,11 +1,11 @@
 ({
     doInit: function(component, event, helper) {
-        var action = component.get("c.getRecurringHelpTexts");
+        const action = component.get("c.getRecurringHelpTexts");
         action.setCallback(this, function(response) {
-            var state = response.getState();
+            const state = response.getState();
 
             if (state === "SUCCESS") {
-                var helpTextMap = response.getReturnValue();
+                const helpTextMap = response.getReturnValue();
 
                 component.set("v.recurringHelpTextMap", helpTextMap);
 
@@ -13,7 +13,7 @@
                 helper.updateRecurringHelpText(component, component.get("v.discountRecord.Recurring_Date_Repeat_By__c"));
 
             } else if (state === "ERROR") {
-                var errors = response.getError();
+                const errors = response.getError();
                 console.error('5. [ERROR] Wystąpiły błędy:', errors);
                 if (errors && errors[0] && errors[0].message) {
                     alert("Błąd podczas pobierania opisów z Apex: " + errors[0].message);
@@ -36,5 +36,60 @@
         let selectedRepeatBy = event.getSource().get("v.value");
         component.set("v.selectedRepeatBy", selectedRepeatBy);
         helper.updateRecurringHelpText(component, selectedRepeatBy);
-    }
+    },
+
+    handleValueTypeChange: function(component, event, helper) {
+        const selectedValue = event.getSource().get("v.value");
+        const discountRecord = component.get("v.discountRecord");
+        discountRecord.Value_Type__c = selectedValue;
+        component.set("v.discountRecord", discountRecord);
+    },
+
+    handleSave: function(component, event, helper) {
+        let discount = component.get("v.discountRecord");
+
+        discount.Name = component.find("discountName").get("v.value");
+        discount.Discount_Value__c = component.find("discountValue").get("v.value");
+        discount.Value_Type__c = component.get("v.selectedValueType");
+        discount.Recurring_Date_Repeat_By__c = component.get("v.selectedRepeatBy");
+        discount.Recurring_Date_Start__c = component.find("recurringStartDate") ? component.find("recurringStartDate").get("v.value") : null;
+        discount.Recurring_Date_End__c = component.find("recurringEndDate") ? component.find("recurringEndDate").get("v.value") : null;
+        discount.Time_Bound_Start__c = component.find("timeBoundStartDate") ? component.find("timeBoundStartDate").get("v.value") : null;
+        discount.Time_Bound_End__c = component.find("timeBoundEndDate") ? component.find("timeBoundEndDate").get("v.value") : null;
+        discount.Conditional_Criteria__c = component.find("conditionalCriteria") ? component.find("conditionalCriteria").get("v.value") : null;
+
+        component.set("v.discountRecord", discount);
+        console.log("Discount Record to Save: ", discount);
+
+        const action = component.get("c.saveDiscount");
+        action.setParams({ discount: discount });
+
+        action.setCallback(this, function(response) {
+            const state = response.getState();
+            if (state === "SUCCESS") {
+                const dismissActionPanel = $A.get("e.force:closeQuickAction");
+                dismissActionPanel.fire();
+                $A.get("e.force:showToast").setParams({
+                    "title": "Success",
+                    "message": "Discount saved successfully.",
+                    "type": "success"
+                }).fire();
+            } else if (state === "ERROR") {
+                const errors = response.getError();
+                const message = (errors && errors[0] && errors[0].message) ? errors[0].message : "Unknown error";
+                $A.get("e.force:showToast").setParams({
+                    "title": "Error",
+                    "message": message,
+                    "type": "error"
+                }).fire();
+            }
+        });
+
+        $A.enqueueAction(action);
+    },
+
+    handleCancel: function(component, event, helper) {
+        const dismissActionPanel = $A.get("e.force:closeQuickAction");
+        dismissActionPanel.fire();
+    },
 });
